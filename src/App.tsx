496bcfe1e3b744bde96c57c82fa85435ead27d92
James Buckhouse <Buckhouse@users.noreply.github.com>
2025-03-23 12:51:45 -0700

    adj for urls and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Gallery from './components/Gallery/Gallery';
 import ArtworkDetail from './components/Gallery/ArtworkDetail';
 import Navbar from './components/Navbar/Navbar';
